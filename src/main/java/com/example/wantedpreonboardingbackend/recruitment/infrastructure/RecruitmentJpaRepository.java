89c444c7629b721257a6866d3a89d509dee24813
kangwongu <kwg0527@naver.com>
2023-10-12 13:35:40 +0900

    Feat: Fix N+1

--- src/main/java/com/example/wantedpreonboardingbackend/recruitment/infrastructure/RecruitmentJpaRepository.java
+++ src/main/java/com/example/wantedpreonboardingbackend/recruitment/infrastructure/RecruitmentJpaRepository.java
@@ -2,14 +2,21 @@ package com.example.wantedpreonboardingbackend.recruitment.infrastructure;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface RecruitmentJpaRepository extends JpaRepository<RecruitmentEntity, Long> {
 
     @Query("select r from RecruitmentEntity r " +
             "join fetch r.company")
     List<RecruitmentEntity> findAll();
+
+    @Query("select r from RecruitmentEntity r " +
+            "join fetch r.company c " +
+            "where r.id = :recruitmentId")
+    Optional<RecruitmentEntity> findById(@Param("recruitmentId") long recruitmentId);
 
     List<RecruitmentEntity> findAllByCompanyId(long companyId);
 }
